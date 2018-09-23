d326024bb4bbf3267f931e70a552d152621d3a36
liliana Quinley <lilicuellar99@gmail.com>
2018-09-22 22:51:10 -0600

    Set Timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,43 @@
 $(document).ready(function () {
 
+
+    var number = 5;
+
+    //  Variable that will hold our interval ID when we execute
+    //  the "run" function
+    var intervalId;
+
+    function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+    }
+    function decrement() {
+
+      //  Decrease number by one.
+      number--;
+      //  Show the number in the #show-number tag.
+      $("#time-left").html(number);
+      //  Once number hits zero...
+      if (number === 0) {
+      //  ...run the stop function.
+        stop();
+        //  Alert the user that time is up.
+        $(".alert-success").html("Time Up! - The correct Answer is:");
+      }
+    }
+
+    //  The stop function
+    function stop() {
+
+      //  Clears our intervalId
+      //  We just pass the name of the interval
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+    }
+
+    //  Execute the run function.
+    run();
+
     
     // $(".btn-primary btn-lg").click(function () {  
     //     resultado.hide(1000);
